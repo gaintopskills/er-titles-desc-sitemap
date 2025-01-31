db5b732d68e3572cd5163b13d668a8ba2d847fa9
gaintopskills <raynafit@yahoo.com>
2025-01-30 20:29:25 -0800

    1

--- src/components/NavbarHome.jsx
+++ src/components/NavbarHome.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import { motion, AnimatePresence } from 'framer-motion';
 import { InstagramIcon } from "../assets/icons/InstagramIcon";
 import { TiktokIcon } from "../assets/icons/TiktokIcon";
-import PhoneIcon from "../phone.png";
+import PhoneIcon from "../assets/icons/phone.png"; 
 
 import { TailcastLogo } from '../assets/logos/TailcastLogo';
 import { GithubIcon } from '../assets/icons/GithubIcon';
@@ -110,7 +110,7 @@ export const NavbarHome = () => {
   href="tel:+13235843000"
   aria-label="Call us"
 >
-  <img src={PhoneIcon} alt="Phone Icon" className="w-5 h-5 mr-2" />
+<img src={PhoneIcon} alt="Phone Icon" className="w-5 h-5 mr-2" />
   (323) 584-3000
 </a>
             <div className="w-36 mx-auto lg:mx-0">
