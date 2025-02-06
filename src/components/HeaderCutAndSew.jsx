b221498a8698129b83dbf2dcbd4cc04c5c72fdfb
gaintopskills <raynafit@yahoo.com>
2025-02-06 10:37:17 -0800

    1

--- src/components/HeaderCutAndSew.jsx
+++ src/components/HeaderCutAndSew.jsx
@@ -37,20 +37,18 @@ export const HeaderCutAndSew = () => {
       window.scrollTo({ top: y, behavior: "smooth" });
     }
   };
-
   return (
     <div className="relative w-full h-screen flex flex-col md:flex-row overflow-hidden">
       {/* 🎥 Video Background */}
       <div className="relative w-full md:w-1/2 h-1/2 md:h-full md:ml-5 overflow-hidden">
       <iframe
   className="w-full h-full absolute inset-0 rounded-lg shadow-[0px_0px_15px_rgba(255,255,255,0.2)]"
-  src="https://www.youtube.com/embed/KYSIemzxIU4?autoplay=1&mute=1&loop=1&controls=1&showinfo=0&playlist=KYSIemzxIU4"
+  src="https://www.youtube.com/embed/KYSIemzxIU4?autoplay=1&mute=1&loop=1&controls=1&modestbranding=1&rel=0&playsinline=1"
   title="Background Video"
-  allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
   loading="lazy"
   style={{ borderRadius: "12px" }}
 ></iframe>
-
       </div>
 
       {/* 🔥 Overlay */}
