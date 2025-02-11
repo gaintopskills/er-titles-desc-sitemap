1fa195225f881101e9a51921ad331292e9b1b8d8
gaintopskills <raynafit@yahoo.com>
2025-02-11 10:59:40 -0800

    1

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,4 +1,4 @@
-import { FacebookIcon } from "../assets/icons/FacebookIcon";
+
 import { InstagramIcon } from "../assets/icons/InstagramIcon";
 import { TailcastLogo } from "../assets/logos/TailcastLogo";
 import { TiktokIcon } from "../assets/icons/TiktokIcon";
